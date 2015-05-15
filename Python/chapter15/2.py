1d4b5813fa8847eced2fe811f0a532db0cf99912
lfwu <zgwulongfei@gmail.com>
2015-05-15 23:58:29 +0800

    add word match

---
+++ Python/chapter15/2.py
@@ -0,0 +1,17 @@
+# this program used to match a group word split with space
+
+
+import re
+
+
+def main():
+    regstr = r"(\w)+ (\w)+"
+    input_str = raw_input("input world to match:")
+    reobj = re.match(regstr, input_str)
+
+    if reobj is not None:
+        print reobj.group()
+
+
+if __name__ == "__main__":
+    main()
